27f6c2fe98ae514753d149cbd8c3842c847f689a
unknown <contatophss11@gmail.com>
2022-12-20 12:56:16 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+
+// Import Routes
+const toughtsRoutes = require('./routes/toughtsRoutes')
+
+const app = express()
+
+const conn = require('./db/conn')
+
+
+// !MODELS
+const Tought = require('./models/Tought')
+const User = require('./models/User')
+
+// import controller showTought para definir a rota / para os pensamentos por padrão
+const ToughtController = require('./controllers/ToughtController')
+
+
+//* Configurando o Handlebars
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+//* Receber respostas do body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+
+//* Session save middleware
+app.use(
+    session({
+        name:"session",
+        secret: "nosso_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function () {},
+            path: require('path').join(require('os').tmpdir(), 'sessions')
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    }),
+)
+
+
+//* flash messages
+app.use(flash())
+
+//* public path
+app.use(express.static('public'))
+
+//* set session to response
+app.use((req, res, next) => {
+    if(req.session.userid){
+        res.locals.session = req.session
+    }
+
+    next()
+
+})
+
+
+//* Routes
+app.use('/toughts', toughtsRoutes)
+app.get('/', ToughtController.showToughts)
+
+conn
+    .sync()
+    .then(() => {
+        app.listen(3000)
+    }).catch((err) => console.log(err))
